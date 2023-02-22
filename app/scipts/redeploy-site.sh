03a9cfee553e0c70f975ecf0f513d4f646c7ce0f
[LauraAurora] <[laura.apenza@gmail.com]>
2023-02-21 21:23:25 -0500

    deploy

--- app/scipts/redeploy-site.sh
+++ app/scipts/redeploy-site.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Reset environment and update project.
-tmux kill-server
-cd "~/project-placeholder"
-git fetch && git reset origin/main --hard
+# 1. Kills all existing tmux sessions.
+tmux kill-server;
 
-# Boot process.
-tmux new-session -d -s process "cd ~/project-placeholder && source venv/bin/activate && pip install -q -r requirements.txt && flask run --host=0.0.0.0";
+# 2. cd into project folder
+cd "~/project-placeholder";
+
+# 3. Run for the latests changes
+git fetch --all && git reset  --hard origin/main;
+
+# 4&5. Python VM and new detached Tmux session
+tmux new-session -d -s process "cd ~/project-placeholder && source venv/bin/activate && 
+pip install -q -r requirements.txt && flask run --host=0.0.0.0";

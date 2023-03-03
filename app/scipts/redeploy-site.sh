ff7ca70942ea1bd5d8599fb30fce2da5dbdf951c
[LauraAurora] <[laura.apenza@gmail.com]>
2023-03-03 11:36:35 -0500

    Changing directory

--- app/scipts/redeploy-site.sh
+++ app/scipts/redeploy-site.sh
@@ -1,14 +1,14 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # 1. Kills all existing tmux sessions.
-# tmux kill-server;
+# 1. Kills all existing tmux sessions.
+tmux kill-server;
 
-# # 2. cd into project folder
-# cd "~/project-placeholder";
+# 2. cd into project folder
+cd "project-placeholder";
 
-# # 3. Run for the latests changes
-# git fetch --all && git reset  --hard origin/main;
+# 3. Run for the latests changes
+git fetch --all && git reset  --hard origin/main;
 
-# # 4&5. Python VM and new detached Tmux session
-# tmux new-session -d -s process "cd ~/project-placeholder && source venv/bin/activate && 
-# pip install -q -r requirements.txt && flask run --host=0.0.0.0";
+# 4&5. Python VM and new detached Tmux session
+tmux new-session -d -s process "cd project-placeholder && source venv/bin/activate && 
+pip install -q -r requirements.txt && flask run --host=0.0.0.0";
